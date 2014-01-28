ab15e7267f1c5c971307a1135a01c3f1d6eff967
Greg <imaspiderman@gmail.com>
2014-01-28 16:04:10 -0500

    Laser model

---
+++ Laser.h
@@ -0,0 +1,8 @@
+const s32 obj[] = {12, //total number of vertices
+	8, //total number of line end points
+	2,
+	//Distinct vertices
+	0,0,4000,-800,0,8000,0,0,8800,800,0,8000
+	
+	//Vertex indexes
+	,0,1,1,2,2,3,3,0};
